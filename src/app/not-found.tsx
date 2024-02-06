a60123b8a6c6c5f2f1a6a6d17113d05cf8be7b38
MrPand-21 <alperacar751@gmail.com>
2024-02-06 14:33:56 +0300

    home-page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,15 @@ export default function NotFound() {
         <div className="text-center h-[70vh] flex flex-col justify-center items-center">
             <p className="text-2xl font-semibold text-[var(--primary-rgb)]">404</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight from-slate-900 to-slate-400 sm:text-5xl
-            bg-gradient-to-br dark:from-violet-500 dark:to-purple-500 bg-clip-text text-transparent">Page not found</h1>
-            <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+            bg-gradient-to-br dark:from-violet-500 dark:to-purple-500 bg-clip-text text-transparent">Sayfa Yko</h1>
+            <p className="mt-6 text-base leading-7 text-gray-600">Aradığın sayfayı bulabilmiş değiliz, şunlardan birini deneyebilirsin ama :</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a href="/" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold 
                 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 
                 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-300 
-                hover:shadow-lg hover:scale-110 hover:shadow-slate-500">Go back home</a>
+                hover:shadow-lg hover:scale-110 hover:shadow-slate-500">Anasayfaya git</a>
                 <a href="/" className="text-sm font-semibold text-gray-900
-                dark:text-gray-100">Contact support <span aria-hidden="true">&rarr;</span></a>
+                dark:text-gray-100">Bizimle iletişime geç <span aria-hidden="true">&rarr;</span></a>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import InstudiApp from '../../public/static/instudiAppPage.png';
 import CrInstudiText from '@/components/CrInstudiText';
 import CrLinkButton from '@/client/CrLinkButton';
 const stats = [
-  { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
-  { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-  { id: 3, name: 'New users annually', value: '46,000' },
+  { id: 1, name: 'Çevrim içi öğrenimin geleneksel yöntemden daha iyi olduğunu söylüyor ', value: 'Öğrencilerin %70\'i', caption: "Potomac Üniversitesi" },
+  { id: 2, name: 'Ayda 1\'den daha az rehberlik servislerinden yararlanmakta', value: '%55\'ten fazlası', caption: "Uluslararası İnsan Bilimleri Dergisi. 10.14687/ijhs.v11i2.2849" },
+  { id: 3, name: 'Türkiye\'de hane halkı tarafından yapılan toplam yıllık eğitim harcaması', value: '148 Milyar TRY', caption: "https://data.tuik.gov.tr/Bulten/Index?p=Egitim-Harcamalari-Istatistikleri-2022-49574" },
 ]
 export default function Home() {
   return (
@@ -72,14 +72,57 @@ export default function Home() {
       lg:right-[38.3%] 2xl:w-20 lg:top-[18%] absolute origin-top-left  bg-[var(--secondary-rgb)]'></div>
       </div>
 
-      <div className="bg-white py-24 sm:py-32">
+      <section className="relative isolate overflow-hidden bg-transparent  px-6 py-24 sm:py-32 lg:px-8">
+        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_center,theme(colors.indigo.500),white)]
+        dark:bg-[radial-gradient(45rem_50rem_at_center,theme(colors.indigo.500),black)] opacity-10" />
+        <div className="absolute inset-y-0 opacity-60 right-[60%] xl:right-[75%] -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-25deg] 
+         shadow-lg shadow-[var(--primary-rgb)] ring-1 sm:mr-28 lg:mr-0 dark:opacity-70
+        xl:mr-16 xl:origin-center" />
+        <div className="absolute opacity-60 inset-y-0 right-[50%] xl:right-[65%] -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[25deg] 
+         shadow-lg shadow-[var(--primary-rgb)] ring-1 sm:mr-28 lg:mr-0 dark:opacity-100
+        xl:mr-16 xl:origin-center" />
+        <div className="mx-auto max-w-2xl lg:max-w-4xl antialiased">
+          <figure className="mt-10">
+            <figcaption className="mb-10">
+              <div className="mt-4 flex items-center justify-center space-x-3 text-2xl">
+                <div className="font-semibold text-gray-800/95 dark:text-[var(--primary-rgb)]">Instudi Neyi Amaçlar?</div>
+                <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-800">
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <div className="text-gray-600 text-lg">Kısaca</div>
+              </div>
+            </figcaption>
+            <blockquote className="text-center text-lg leading-8 text-gray-800/95 sm:text-2xl sm:leading-9 dark:text-slate-300/80">
+              <p>
+                Instudi, öğrencilerin akış kısmında birbirlerinden faydalanabileceği, mentörlük sayfasında üniversitelilerden ve
+                öğretmenlerden birebir görüşme veya sınıf dersi alabileceği, eğitim kurumlarını inceleyip webinarlara katılabilecekleri
+                ayrıca tüm dersler hakkında podcastler dinleyebilecekleri modüler bir platformdur. Eğitimde:
+              </p>
+            </blockquote>
+
+          </figure>
+        </div>
+      </section>
+
+      <div className="py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
             {stats.map((stat) => (
               <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-                <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
-                <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                <dt className="text-base leading-7 text-gray-600/95 dark:text-slate-500">{stat.name}
+                </dt>
+                <dd className="order-first relative text-3xl font-semibold tracking-tight text-gray-800/95 sm:text-5xl
+                dark:bg-gradient-to-br dark:from-blue-800 dark:to-blue-500 dark:bg-clip-text dark:text-transparent">
                   {stat.value}
+                  <span className='group'>
+                    *
+                    <div id="tooltip-default" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-xs scale-0
+                    group-hover:scale-100 font-light tracking-normal shadow-slate-400 left-1/2 -translate-x-1/2 group-hover:-top-[50%] top-1/2 w-fit 
+                   text-[var(--background-start-rgb)] dark:text-slate-400 transition-all duration-300 bg-[var(--foreground-text-rgb)] rounded-lg shadow-lg opacity-30 dark:bg-gray-700 
+                   group-hover:visible group-hover:opacity-100  dark:shadow-blue-700/70">
+                      {stat.caption}
+                    </div>
+                  </span>
                 </dd>
               </div>
             ))}
@@ -87,35 +130,7 @@ export default function Home() {
         </div>
       </div>
 
-      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-        <div className="mx-auto max-w-2xl lg:max-w-4xl">
-          <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
-          <figure className="mt-10">
-            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-              <p>
-                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                molestiae. Numquam corrupti in laborum sed rerum et corporis.”
-              </p>
-            </blockquote>
-            <figcaption className="mt-10">
-              <img
-                className="mx-auto h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                <div className="font-semibold text-gray-900">Judith Black</div>
-                <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
-                <div className="text-gray-600">CEO of Workcation</div>
-              </div>
-            </figcaption>
-          </figure>
-        </div>
-      </section>
+
     </main>
   );
 }
